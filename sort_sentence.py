a512c1e021b230ac33f83d642c77b0b21631585d
Henok Matheas Kebede <henokmatheaskebede@gmail.com>
2021-12-11 10:03:08 +0300

    sort_sentence file added

---
+++ sort_sentence.py
@@ -0,0 +1,27 @@
+
+
+s = "Myself2 Me1 I4 and3"
+
+
+def sortSentence(s):
+    listi = [0]*8
+    final = ""
+    temp = " "
+    for  i in s:
+        try:
+            inti = int(i)
+            listi[inti-1] = temp
+            temp = " "
+        except:
+            if i != " ":
+                temp += i
+                print(temp)
+    for i in listi:
+        if i != 0:
+            final += i+" "
+
+    return final.rstrip()
+
+
+sortSentence(s)
+print(sortSentence(s))
