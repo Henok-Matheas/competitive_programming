9212a58b83a4206a3dfafa9263357c19bcd29363
Henok Matheas Kebede <henokmatheaskebede@gmail.com>
2021-12-11 10:55:00 +0300

    method to find the number of smaller elements in list

---
+++ week_1/smallerNumbersThanCurrent.py
@@ -0,0 +1,15 @@
+
+
+def smallerNumbersThanCurrent(nums):
+    #this is the bruteforced version
+    smallerNumbersList= [0]*len(nums)
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            if nums[j] < nums[i]:
+                smallerNumbersList[i] +=1
+    return smallerNumbersList
+
+
+nums = [8,1,2,2,3]
+
+print(smallerNumbersThanCurrent(nums))
