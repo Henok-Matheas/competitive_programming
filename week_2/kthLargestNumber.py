1404babcf5e88baa39072a4bce567b970203b58c
Henok Matheas Kebede <henokmatheaskebede@gmail.com>
2021-12-15 06:32:16 +0300

    kthlargestNumber fix

--- week_2/kthLargestNumber.py
+++ week_2/kthLargestNumber.py
@@ -10,4 +10,16 @@ def kthLargestNumber(nums, k):
                 low_index = j
         nums[i], nums[low_index] = nums[low_index], nums[i]
     
-    return str(nums[len(nums[k])])
+    return str(nums[len(nums)-k])
+
+
+# nums = ["2","21","12","1"]
+# k = 3
+# # Output: "2"
+
+nums = ["3","6","7","10"]
+k = 4
+# Output: "3"
+
+print(kthLargestNumber(nums,k))
+print(type(kthLargestNumber(nums,k)))
