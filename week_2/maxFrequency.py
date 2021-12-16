e1adfc9b3742503646bcdb5c2a579670423e7a9f
Henok Matheas Kebede <henokmatheaskebede@gmail.com>
2021-12-16 18:52:54 +0300

    maxfrequency final

--- week_2/maxFrequency.py
+++ week_2/maxFrequency.py
@@ -1,20 +1,19 @@
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int) -> int:
+        nums.sort(reverse = True)
+        maxim = 1
+        i, j = 0, 0
+        
+        n = len(nums) -1
 
+        while j<n:
+            diff = nums[i] - nums[j + 1]
 
-def maxFrequency(nums,k):
-    nums.count(reverse = True)
-    maxim = 1
-    i, j = 0, 1
-
-    while j<n:
-        diff = nums[i] - nums[j]
-
-        if diff <= k:
-            k -= diff
-            maxim = max(maxim, (j - i))
-            j +=1 
-        else:
-            k += (nums[i] - nums[i + 1])(j - i)
-            i += 1
-
-
-print(maxFrequency([]))
+            if diff <= k:
+                k -= diff
+                j +=1 
+                maxim = max(maxim, (j - i + 1))
+            else:
+                k += (nums[i] - nums[i + 1]) * (j - i)
+                i += 1
+        return maxim
