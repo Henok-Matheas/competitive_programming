549ec2514f79e8615fc659f79caedadbdaf4b7b1
Henok Matheas Kebede <henokmatheaskebede@gmail.com>
2021-12-18 16:06:06 +0300

    finalized number-of-recent-calls

--- week_2/recentCalls.py
+++ week_2/recentCalls.py
@@ -1,19 +1,22 @@
 class RecentCounter:
 
     def __init__(self):
-        self.pings = []
+        self.queue = []
+        self.index = 0
         
 
     def ping(self, t: int) -> int:
-        cnt = 0
-        (self.pings).append(t)
-        for j in range(len(self.pings), 0, -1):
-            if self.pings[j - 1] > t - 3001:
-                cnt += 1
-                # print(t, self.pings[j - 1],cnt)
-            else:
-                break
-        return cnt
+        
+        (self.queue).append(t)
+        for i in range(self.index, len(self.queue)):
+            if self.queue[i] >= self.queue[-1] - 3000:
+                self.index = i
+                return len(self.queue) - self.index
+        
+        
+        
+        
+        
         
         
 
