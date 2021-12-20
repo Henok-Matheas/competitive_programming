1b3d4b255aee0f188bcb47f7905943eb3762802e
Henok Matheas Kebede <henokmatheaskebede@gmail.com>
2021-12-20 10:42:44 +0300

    max-number-of-k-sum-pairs

---
+++ week_2/maxOperations.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        
+        nums.sort()
+        operations = 0
+        i, j = 0, len(nums) - 1
+
+        while i < j:
+            if nums[i] + nums[j] == k:
+                operations +=1
+                i += 1
+                j -= 1
+            elif nums[i] + nums[j] < k:
+                i += 1
+            else:
+                j -= 1
+        return operations
