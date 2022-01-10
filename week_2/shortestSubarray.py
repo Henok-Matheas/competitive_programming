e1e91f5664826b187bccc6a221bb794b5e386a23
Henok Matheas Kebede <henokmatheaskebede@gmail.com>
2022-01-10 15:50:16 +0300

    shortest subarray

--- week_2/shortestSubarray.py
+++ week_2/shortestSubarray.py
@@ -1,27 +1,56 @@
-
-def shortestSubarray(nums, k):
-    nums.sort()
-        
-    leng = -1
-        
-    if len(nums) < 2:
-        if nums[-1] >= k:
-            return 1
-        else:
-            return -1
-        
-    for i in range(1, len(nums)):
-        nums[i] +=nums[i - 1]
-        if nums[i] >= k:
-            return i + 1
+from typing import Deque
 
 
-# nums = [1]
-# k = 1
-# # Output: 1
+class Solution:
+    def shortestSubarray(self, nums: List[int], k: int) -> int:
 
-nums = [2,-1,2]
-k = 3
-# Output: 3
+        count = -1
 
-print(shortestSubarray(nums,k))
+        prefix_sum = [0]
+        mono_stack = [0]
+
+        p_sum = 0
+        for index in range(len(nums)):
+            if nums[index] >= k:
+                return 1
+            p_sum += nums[index]
+            prefix_sum.append(p_sum)
+
+        deq = Deque()
+        temp_val = None
+
+        print("the prefix sum is ", prefix_sum)
+        for index in range(len(prefix_sum)):
+            # print("this is the current value of the prefix_sum",prefix_sum[index])
+            if len(deq) == 0:
+                deq.append(index)
+            while len(deq) and prefix_sum[index] - prefix_sum[deq[0]] >= k:
+                val = deq.popleft()
+                # print(val)
+                # print(prefix_sum[val],prefix_sum[index])
+                if temp_val == None:
+                    temp_val = index - val
+                else:
+                    temp_val = min(temp_val, index - val)
+            while len(deq) and prefix_sum[index] <= prefix_sum[deq[-1]]:
+                # print(deq,'this is deq')
+                deq.pop()
+            deq.append(index)
+        print("this is the appended deq", deq)
+
+        # i = 0
+        # temp_val = None
+        # for index in range(len(prefix_sum)):
+        #     while prefix_sum[index] - prefix_sum[mono_stack[i]] >= k and i < len(mono_stack):
+        #         if temp_val == None:
+        #             temp_val = index - i
+        #         else:
+        #             temp_val = min(temp_val, index - i)
+        #         i += 1
+        #     while len(mono_stack) > 0 and prefix_sum[index] <= prefix_sum[mono_stack[-1]]:
+        #         mono_stack.pop()
+        #         i -= 1
+        #     mono_stack.append(index)
+        #     i += 1
+
+        return temp_val if temp_val != None else -1
