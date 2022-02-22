cef140fd6eeb06599f26dd5df1610ed1e2642ec1
Henok Matheas Kebede <henokmatheaskebede@gmail.com>
2022-02-22 15:08:35 +0300

    final heap implementation

--- week_7/heap.py
+++ week_7/heap.py
@@ -3,7 +3,6 @@ class Solution:
     def heapify(self, arr, n, i):
         idx = i
         while self.parent(i) > -1 and arr[self.parent(i)] > arr[i]:
-            print("upheap")
             arr[self.parent(i)], arr[i] = arr[i], arr[self.parent(i)]
             i = self.parent(i)
 
@@ -11,39 +10,11 @@ class Solution:
                ) or (self.rightChild(idx) < n
                      and arr[self.rightChild(idx)] < arr[idx]):
             minChildIndex = self.leftChild(idx)
-            print("downheap")
             if self.rightChild(idx) < n and arr[self.rightChild(
                     idx)] < arr[minChildIndex]:
-                print("downheap rightChild", arr[self.rightChild(idx)])
                 minChildIndex = self.rightChild(idx)
             arr[idx], arr[minChildIndex] = arr[minChildIndex], arr[idx]
             idx = minChildIndex
-
-    def downer(self, arr, n, i):
-        idx = i
-        print(
-            "#################################downer###############################"
-        )
-        while (self.lefter(idx) < n and arr[self.lefter(idx)] < arr[idx]) or (
-                self.righter(idx) < n and arr[self.righter(idx)] < arr[idx]):
-            print("got in")
-            minChildIndex = 0
-            if arr[self.lefter(idx)] < arr[idx]:
-                minChildIndex = self.lefter(idx)
-            if self.righter(idx) < n and arr[self.righter(
-                    idx)] < arr[minChildIndex]:
-                minChildIndex = self.righter(idx)
-            arr[idx], arr[minChildIndex] = arr[minChildIndex], arr[idx]
-            idx = minChildIndex
-            self.heapify(arr, len(arr), idx)
-            print("this is the index", idx, "this is the value", arr[idx])
-
-    def lefter(self, index):
-        return index + 1
-
-    def righter(self, index):
-        return index + 2
-        # code here
 
         #Function to build a Heap from array.
     def buildHeap(self, arr, n):
