238c4d9cd2412b85a86b49ca5f5e8292279e4061
Henok Matheas Kebede <henokmatheaskebede@gmail.com>
2022-02-23 19:34:19 +0300

    mergeKList

--- week_7/mergeKLists.py
+++ week_7/mergeKLists.py
@@ -2,30 +2,73 @@ import heapq
 
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class Node:
+
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def __lt__(self, other):
+        return self.val < other.val
+
+
 class Solution:
 
     def mergeKLists(self,
                     lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        whole = []
 
-        for list in lists:
-            while list != None:
-                whole.append(list.val)
-                list = list.next
+        l_list = []
 
-        heapq.heapify(whole)
-
-        node = ListNode(heapq.heappop(whole)) if whole else None
-        if not node:
-            return node
-        begin = node
-
-        n = len(whole)
-        for index in range(n):
-            node.next = ListNode(heapq.heappop(whole))
+        for node in lists:
+            if node:
+                heapq.heappush(l_list, Node(node.val, node.next))
+        merged = Node("")
+        if l_list:
+            node = heapq.heappop(l_list)
+            merged = Node(node.val)
             node = node.next
-        return begin
+            if node:
+                heapq.heappush(l_list, Node(node.val, node.next))
+
+        merger = merged
+
+        while l_list:
+            node = heapq.heappop(l_list)
+            merged.next = Node(node.val)
+            merged = merged.next
+            node = node.next
+            if node:
+                heapq.heappush(l_list, Node(node.val, node.next))
+
+        return merger
+
+
+# import heapq
+# # Definition for singly-linked list.
+# # class ListNode:
+# #     def __init__(self, val=0, next=None):
+# #         self.val = val
+# #         self.next = next
+# class Solution:
+
+#     def mergeKLists(self,
+#                     lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+#         whole = []
+
+#         for list in lists:
+#             while list != None:
+#                 whole.append(list.val)
+#                 list = list.next
+
+#         heapq.heapify(whole)
+
+#         node = ListNode(heapq.heappop(whole)) if whole else None
+#         if not node:
+#             return node
+#         begin = node
+
+#         n = len(whole)
+#         for index in range(n):
+#             node.next = ListNode(heapq.heappop(whole))
+#             node = node.next
+#         return begin
