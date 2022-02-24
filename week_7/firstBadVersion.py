a630caf77acb063b32f05ac1e2985757dc0f520b
Henok Matheas Kebede <henokmatheaskebede@gmail.com>
2022-02-24 19:03:19 +0300

    first bad version

---
+++ week_7/firstBadVersion.py
@@ -0,0 +1,20 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+
+class Solution:
+
+    def firstBadVersion(self, n: int) -> int:
+        left = 1
+        right = n
+        first = 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if not isBadVersion(mid):
+                left = mid + 1
+            else:
+                right = mid - 1
+                first = mid
+        return first
