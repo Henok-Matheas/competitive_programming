43a307bf5a5218ea591a18e72601241e25def03a
Henok Matheas Kebede <henokmatheaskebede@gmail.com>
2022-03-03 11:15:38 +0300

    surrounded region

--- week_8/surroundedRegions.py
+++ week_8/surroundedRegions.py
@@ -1,44 +1,45 @@
 class Solution:
 
-
     def solve(self, board: List[List[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
-"""
+        """
 
-        # left, right, up , down     
-directions = [[0, -1], [0, 1], [-1, 0], [1, 0]]
- 
+        # left, right, up, down
+        directions = [[0, -1], [0, 1], [-1, 0], [1, 0]]
+
         def right(row, column):
             for i in range(column):
                 if board[0][i] == "O":
                     recur(0, i)
                 if board[row - 1][i] == "O":
+                    recur(row - 1, i)
 
-        def down(row,  column):
+        def down(row, column):
             for i in range(row):
                 if board[i][0] == "O":
-                    recur(i,  0)
+                    recur(i, 0)
                 if board[i][column - 1] == "O":
                     recur(i, column - 1)
 
-        bound = lambda row, column row < len(
-            
-
+        bound = lambda row, column: row < len(
             board) and row >= 0 and column < len(board[0]) and column >= 0
 
-       def rr(rw, cl):
-          board[][cl] = "Y"
-         for dirction in directions:
-            row = rw + direction[ 0]
-                colu cl + direction[1]
-                ot bound(r ow, column) or board[row][column] != "O":
-rw = len(board)
+        def recur(rw, cl):
+            board[rw][cl] = "Y"
+            for direction in directions:
+                row = rw + direction[0]
+                column = cl + direction[1]
+                if not bound(row, column) or board[row][column] != "O":
+                    continue
+                recur(row, column)
+
+        rw = len(board)
         cl = len(board[0])
         right(rw, cl)
-        down(rw,  cl)
- 
-for row in range(rw):
+        down(rw, cl)
+
+        for row in range(rw):
             for column in range(cl):
                 if board[row][column] == "O":
                     board[row][column] = "X"
