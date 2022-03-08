8d77b15f4e04864055f5a8574ab813f2e67b1c79
Henok Matheas Kebede <henokmatheaskebede@gmail.com>
2022-03-08 12:50:31 +0300

    num rescue boats

--- week_9/numRescueBoat.py
+++ week_9/numRescueBoat.py
@@ -22,3 +22,20 @@ class Solution:
             minheap.pop()
             count += 1
         return count
+
+        people.sort()
+
+        ppl = collections.deque(people)
+
+        count = 0
+        while ppl:
+            if len(ppl) == 1:
+                return count + 1
+
+            first = ppl[0]
+            last = ppl.pop()
+
+            if first + last <= limit:
+                ppl.popleft()
+            count += 1
+        return count
