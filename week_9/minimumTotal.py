5d6babbde812e36347d3265019444c4ecd4eb1e4
Henok Matheas Kebede <henokmatheaskebede@gmail.com>
2022-03-10 14:08:24 +0300

    triangle

--- week_9/minimumTotal.py
+++ week_9/minimumTotal.py
@@ -1,6 +1,26 @@
 class Solution:
 
     def minimumTotal(self, triangle: List[List[int]]) -> int:
+        tot = float("inf")
+        for rowindx in range(len(triangle)):
+            minim = float("inf")
+            for indx in range(len(triangle[rowindx])):
+                tplft = 10000000 if indx == 0 or rowindx == 0 else triangle[
+                    rowindx - 1][indx - 1]
+                tp = 10000000 if indx == len(
+                    triangle[rowindx]) - 1 or rowindx == 0 else triangle[
+                        rowindx - 1][indx]
+
+                triangle[rowindx][indx] = triangle[rowindx][indx] + (min(
+                    tplft, tp) if tplft != 10000000 or tp != 10000000 else 0)
+                minim = min(minim, triangle[rowindx][indx])
+            tot = minim
+        return tot
+
+
+class Solution:
+
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
         visited = defaultdict(bool)
 
         def recur(row, column):
