c12c92d53189291ddfc720f89478b7826de657c9
Henok Matheas Kebede <henokmatheaskebede@gmail.com>
2022-03-10 15:13:53 +0300

    rob

--- week_9/rob.py
+++ week_9/rob.py
@@ -17,3 +17,19 @@ class Solution:
             return max(visited[index + 2], visited[index + 3]) + nums[index]
 
         return max(recur(0), recur(1))
+
+
+class Solution:
+
+    def rob(self, nums: List[int]) -> int:
+
+        maxim = 0
+        bound = lambda row: row >= 0 and row < len(nums)
+        for i in range(len(nums)):
+            jump2 = nums[i - 2] if bound(i - 2) else 0
+            jump3 = nums[i - 3] if bound(i - 3) else 0
+
+            nums[i] += max(jump2, jump3)
+
+            maxim = max(maxim, nums[i])
+        return maxim
