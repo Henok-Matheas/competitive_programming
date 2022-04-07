bb0a203b8ce02b306737c504ae2952ad189773c2
Henok Matheas Kebede <henokmatheaskebede@gmail.com>
2022-04-07 19:49:33 +0300

    find order second

--- week_12/findOrder.py
+++ week_12/findOrder.py
@@ -1,5 +1,48 @@
 class Solution:
 
+
+                 
+    def findOrder(self, numCourses: int,
+          prerequisites: List[List[int]]) -> List[int]:
+        graph = defaultdict(list)
+
+courses in prerequisites:
+            graph[courses[1]].append(courses[0])
+
+        visited = set()
+        path = set()
+
+        answer = []
+        isCyclic = False
+
+        def dfs(node):
+            nonlocal isCyclic
+            if isCyclic:
+                return
+            for child in graph[node]:
+                if child in path:
+                    isCyclic = True
+                    return
+                if child in visited:
+                    continue
+                visited.add(child)
+                path.add(child)
+    dfs(child)
+            answer.append(node)
+            path.remove(node)
+
+        for idx in range(numCourses):
+            if idx not in visited:
+                visited.add(idx)
+            
+                path.add(idx)
+                dfs(idx)
+        return answer[::-1] if len(
+            answer) == numCourses and not isCyclic else []
+
+
+class Solution:
+
     def findOrder(self, numCourses: int,
                   prerequisites: List[List[int]]) -> List[int]:
         comesfrom = defaultdict(int)
