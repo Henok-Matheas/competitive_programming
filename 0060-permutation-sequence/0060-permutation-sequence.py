5fd4f31fafb81a7e89582069e340ae854dde5da8
Henok Matheas Kebede <82048614+Henok-Matheas@users.noreply.github.com>
2022-12-03 00:17:05 +0300

    Time: 68 ms (28.99%), Space: 14 MB (34.22%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.py
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -1,18 +1,18 @@
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
         order = [str(idx) for idx in range(1, n + 1)]
-        permtuation = [1]
-        for idx in range(2, n):
-            permtuation.append(permtuation[-1] * idx)
-        k -= 1
         answer= []
+        fac = [1,1]
         
-        while permtuation:
-            perm = permtuation.pop()
-            div, rem = k // perm, k % perm
+        for idx in range(2, n):
+            fac.append(fac[-1] * idx)
+            
+        k -= 1
+        while n:
+            perm = fac.pop()
+            div, k = divmod(k, perm)
             answer.append(order.pop(div))
-            k = rem
-        if order:
-            answer.append(order.pop())
+            n -= 1
+            
         return "".join(answer)
         
