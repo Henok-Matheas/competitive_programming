6bb416cc8f0bf98e01d5e591cfb8861b319f513a
Henok Matheas Kebede <82048614+Henok-Matheas@users.noreply.github.com>
2023-01-23 08:41:19 +0300

    Time: 695 ms (60.41%), Space: 30.1 MB (76.72%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -7,19 +7,36 @@ class Solution:
                 left, right = left + 1, right - 1
             return True
         
-        @lru_cache(None)
-        def dp(index):
-            if index == len(s):
-                return [[]]
-            answer = []
-            for j in range(index, len(s)):
-                if not isPalindrome(index, j):
-                    continue
-                for lst in dp(j + 1):
-                    answer.append([s[index: j + 1]] + lst)
-            return answer
+        # this is a backtrack question.
+        
+        answers = []
+        path = []
+        def backtrack(start):
+            if start == len(s):
+                answers.append(path[::])
+                
+            for idx in range(start, len(s)):
+                ## check if it's a palindrome
+                if isPalindrome(start, idx):
+                    path.append(s[start: idx + 1])
+                    backtrack(idx + 1)
+                    path.pop()
+        backtrack(0)
+        return answers
+        
+#         @lru_cache(None)
+#         def dp(index):
+#             if index == len(s):
+#                 return [[]]
+#             answer = []
+#             for j in range(index, len(s)):
+#                 if not isPalindrome(index, j):
+#                     continue
+#                 for lst in dp(j + 1):
+#                     answer.append([s[index: j + 1]] + lst)
+#             return answer
         
         
-        return dp(0)
+#         return dp(0)
         
         
