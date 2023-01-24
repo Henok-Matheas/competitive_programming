2ab881e1e7fe5cb2fa22c877db8018605666ce44
Henok Matheas Kebede <82048614+Henok-Matheas@users.noreply.github.com>
2023-01-24 16:20:53 +0300

    Time: 46 ms (35.57%), Space: 13.8 MB (66.11%) - LeetHub

--- 0738-monotone-increasing-digits/0738-monotone-increasing-digits.py
+++ 0738-monotone-increasing-digits/0738-monotone-increasing-digits.py
@@ -6,25 +6,16 @@ class Solution:
         so for every number if it's greater than th
         """
         
-        final = [0]
-        string = str(n)
-        for idx in range(len(string)):
-            num = int(string[idx] )
-            if num >= final[-1]:
-                final.append(num)
-                continue
-            next_ = 9
-            for rev in range(len(final) - 1, 0, -1):
-                final[rev] -= 1
-                curr, prev = final[rev], final[rev - 1]
-                if not(prev <= curr <= next_):
-                    final[rev] = 9
-                    next_ = final[rev]
-                else:
-                    break
-                    
-            final += [9] * (len(string) - idx)
-            break
+        final = [int(char) for char in str(n)]
+        marker = len(final)
+        for idx in range(len(final) - 1, 0, -1):
+            curr, prev = final[idx], final[idx - 1]
+            if curr < prev:
+                marker = idx
+                final[idx - 1] -= 1
+                
+        for idx in range(marker, len(final)):
+            final[idx] = 9
             
             
         return int("".join(list(map(str, final))))
