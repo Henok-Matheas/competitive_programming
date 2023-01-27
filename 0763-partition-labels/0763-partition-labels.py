2c3893e9589f2b34d363acad0c61ae54cd993912
Henok Matheas Kebede <82048614+Henok-Matheas@users.noreply.github.com>
2023-01-27 18:15:57 +0300

    Time: 30 ms (99.03%), Space: 13.8 MB (97.33%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.py
@@ -0,0 +1,28 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        have = set()
+        left = 0
+        output = []
+        
+        """
+        have a dictionary and for every index if the index is less than the last end
+        then we continue else since we are starting again
+        
+        or rather than making it when it is greater we can do it when it is equal
+        
+        """
+        
+        ends = {}
+        start = 0
+        end = 0
+        output = []
+        for idx in range(len(s)):
+            ends[s[idx]] = idx
+        
+        print(ends)
+        for idx in range(len(s)):
+            end = max(end, ends[s[idx]])
+            if idx == end:
+                output.append(idx - start + 1)
+                start = end + 1
+        return output
