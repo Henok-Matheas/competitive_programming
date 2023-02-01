d69476a4d4bc9ec77a1b89d7faf51bc973d667fd
Henok Matheas Kebede <82048614+Henok-Matheas@users.noreply.github.com>
2023-02-01 17:11:31 +0300

    Time: 2144 ms (52.45%), Space: 14.3 MB (51.75%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
@@ -1,15 +1,20 @@
 class Solution:
     def bestTeamScore(self, scores: List[int], ages: List[int]) -> int:
-                                                #   Example: scores = [4,4,6,5]
-                                                #              ages = [2,1,5,1]
-
-        dp = [0]*(1+max(ages))                  #         dp = [0, 0, 0, 0, 0]       
-                                                #  score_age = [(4,1), (4,2), (5,1), (6,5)]
-        score_age = sorted(zip(scores, ages))
-                                                #   score   age     dp
-        for score, age in score_age:            #   –––––  –––––    ––––––––––––––––––
-                                                #     4      1      [0, 4, 0, 0, 0, 0]
-            dp[age] = score + max(dp[:age+1])   #     4      2      [0, 4, 8, 0, 0, 0]
-                                                #     5      1      [0, 9, 8, 0, 0, 0]
-        return max(dp)                          #     6      5      [0, 9, 8, 0, 0,15] 
-                       
+        """
+        conflict is when
+        
+        young player          old player
+        score          >      score
+        """
+        indices = [idx for idx in range(len(ages))]
+        indices.sort(key = lambda idx: (ages[idx], scores[idx]))
+        total_scores = [scores[idx] for idx in indices]
+        
+        for start_idx, start in enumerate(indices):
+            for end_idx in range(start_idx + 1, len(indices)):
+                end = indices[end_idx]
+                ## granted that either if curr_score <= end_score
+                if scores[start] <= scores[end]:
+                    total_scores[end_idx] = max(total_scores[end_idx], total_scores[start_idx] + scores[end])
+        
+        return max(total_scores)
