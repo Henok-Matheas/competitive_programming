a5e49f3bafea1af9157634d979bd4432b00b2c8c
Henok Matheas Kebede <82048614+Henok-Matheas@users.noreply.github.com>
2023-02-02 14:28:05 +0300

    Time: 61 ms (27.94%), Space: 13.9 MB (68.24%) - LeetHub

--- 0937-reorder-data-in-log-files/0937-reorder-data-in-log-files.py
+++ 0937-reorder-data-in-log-files/0937-reorder-data-in-log-files.py
@@ -1,48 +1,16 @@
 class Solution:
+    def ordering(self, log):
+        is_digit = 0
+        id_, content = log.split(" ", maxsplit = 1)
+
+        for word in content:
+            is_digit |= int(word.isdigit())
+
+        if is_digit:
+            id_, content = "", []
+            
+        return (is_digit, content, id_)
+    
     def reorderLogFiles(self, logs: List[str]) -> List[str]:
-        """
-        given logs
-        log
-            - letter log(all lowercase except id)
-            - digit log(all digits)
-            
-        reoder them
-        letterlogs first
-        letterlogs by lexicography(content, id)
-        digit logs(maintain relative ordering)
-        
-        order
-        
-        letterlogs smallercontent
-        letterlogs smallerid
-        digitslogs id ->
-        
-        if it's letterlog(0)
-        digitlog(1)
-        
-        in letterlogs(content, id)
-        """
-        
-        def ordering(value):
-            index, log = value
-            is_digit = 0
-            log_list = log.split()
-            id_ = log_list[0]
-            content = []
-            
-            for idx in range(1, len(log_list)):
-                word = log_list[idx]
-                is_digit |= int(word.isdigit())
-                content.append(word)
-            
-            if is_digit:
-                id_, content = "", []
-            return (is_digit,content, id_,index)
-        
-        idx_logs = [(idx, log) for idx, log in enumerate(logs)]
-        
-        idx_logs.sort(key = lambda value: ordering(value))
-        
-        new_logs = list(map(lambda value: value[1], idx_logs))
-        
-        return new_logs
+        logs.sort(key = lambda log: self.ordering(log))
+        return logs
