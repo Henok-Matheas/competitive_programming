c4709bfef1268e0631560bbc1af2e21bfba84ce6
Henok Matheas Kebede <82048614+Henok-Matheas@users.noreply.github.com>
2023-02-03 17:17:14 +0300

    Time: 205 ms (90.26%), Space: 34.9 MB (5.09%) - LeetHub

---
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.py
@@ -0,0 +1,36 @@
+class Solution:
+    """
+    weights
+    
+    pickIndex randomly picks index and returns it
+    probability[i] = w[i] / sum(w)
+    what if we create a list of size sum(w)
+    
+    we just need to know the sum and the weight
+    
+    so after we know that based on the ratio of sum to weight 
+    we will have a new array
+    
+    so for every index
+     we will append a new array of size int(weight / sum * len(array))
+    """
+
+    def __init__(self, w: List[int]):
+        ## list fo indices
+        self.array = []
+        self.SUM = sum(w)
+        self.LENGTH = len(w)
+        self.PER = 100
+        
+        for idx, weight in enumerate(w):
+            self.array += [idx] * math.ceil(weight * self.LENGTH * self.PER / self.SUM)
+        
+
+    def pickIndex(self) -> int:
+        return self.array[random.randrange(len(self.array))]
+        
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
