3c2bc6e164488b2058116c95e1e86baae891b792
Henok Matheas Kebede <82048614+Henok-Matheas@users.noreply.github.com>
2023-02-07 19:54:11 +0300

    Time: 50 ms (62.45%), Space: 14.1 MB (30.80%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.py
+++ 1598-crawler-log-folder/1598-crawler-log-folder.py
@@ -1,27 +1,6 @@
 class Solution:
     def minOperations(self, logs: List[str]) -> int:
-        """
-        ../move to parent folder
-        ./ remain in the same folder
-        x/ move to the child folder named x(always exists)
-        
-        
-        given logs 
-        filesystem starts in the main folder then logs are perfomed
-        return min number of operations needed to go back to the main folder
-        after the changefolder operations have been done.
-        
-        
-        how to do it:
-        
-        for every log
-        if the log is ../ step -= 1 if step
-        if the log is ./ step += 0
-        if the log is else step += 1
-        """
-        
         step = 0
-        
         for log in logs:
             if log == "../":
                 step -= min(1, step)
