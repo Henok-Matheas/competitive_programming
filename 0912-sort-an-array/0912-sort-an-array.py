2a6502ef710bce79272daa10819e294744d7e258
Henok Matheas Kebede <82048614+Henok-Matheas@users.noreply.github.com>
2023-03-28 09:38:44 +0300

    Time: 1825 ms (54.58%), Space: 23.6 MB (20.46%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,5 +1,24 @@
 class Solution:
+    def merger(self, nums1, nums2):
+        nums = []
+        right = 0
+        
+        for num in nums1:
+            while right < len(nums2) and nums2[right] <= num:
+                nums.append(nums2[right])
+                right += 1
+            
+            nums.append(num)
+        
+        return nums + nums2[right:]
+    
     def sortArray(self, nums: List[int]) -> List[int]:
-        nums.sort()
-        return nums
+        if len(nums) == 1:
+            return nums
+        
+        left = self.sortArray(nums[:len(nums) // 2])
+        right = self.sortArray(nums[len(nums) // 2:])
+        
+        return self.merger(left, right)
+        
     
