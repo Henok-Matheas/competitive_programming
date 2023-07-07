63bda68fcb1b4e7cc96fd7c4c4ac2c7adecc6a49
Henok Matheas Kebede <82048614+Henok-Matheas@users.noreply.github.com>
2023-07-07 15:00:46 +0300

    Time: 412 ms (66.77%), Space: 16.8 MB (55.49%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.py
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.py
@@ -1,55 +1,11 @@
 class Solution:
-    def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
-        """
-        increase the number of same answers in a row
-        
-        answerKey - actual answer
-        k - max operations
-        
-        
-        operation = change answer
-        
-        
-        return max no of T or F possible
-        
-        
-        
-        SOLUTION
-        counter(answer)
-            
-            while not k and left != answer:
-                if left == answer:
-                    continue
-                else:
-                    k += 1
-                count -= 1
-            
-            if current != answer:
-                k -= 1
-                count += 1
-                
-            max_count = max(count, max_count)
-        
-        """
-        
-        
-        def counter(DESIRED, k):
-            max_count = 0
-            left = 0
-            count = 0
-            for current in answerKey:
-                while not k and current != DESIRED:
-                    if answerKey[left] != DESIRED:
-                        k += 1
-                    
-                    left += 1
-                    count -= 1
-                    
-                if current != DESIRED:
-                    k -= 1
-                count += 1
-                max_count = max(max_count, count)
-                
-            return max_count
-        
-        return max(counter("T", k), counter("F", k))
+    def maxConsecutiveAnswers(self, s: str, k: int) -> int:
+        maxf = i = 0
+        count = collections.Counter()
+        for j in range(len(s)):
+            count[s[j]] += 1
+            maxf = max(maxf, count[s[j]])
+            if j - i + 1 > maxf + k:
+                count[s[i]] -= 1
+                i += 1
+        return len(s) - i
