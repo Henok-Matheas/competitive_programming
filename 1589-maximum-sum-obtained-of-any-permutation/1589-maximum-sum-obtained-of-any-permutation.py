5228d901d8a7696b144f08e628aad8e1467aa806
Henok Matheas Kebede <82048614+Henok-Matheas@users.noreply.github.com>
2023-07-25 15:41:20 +0300

    Time: 1629 ms (77.44%), Space: 50.5 MB (49.62%) - LeetHub

--- 1589-maximum-sum-obtained-of-any-permutation/1589-maximum-sum-obtained-of-any-permutation.py
+++ 1589-maximum-sum-obtained-of-any-permutation/1589-maximum-sum-obtained-of-any-permutation.py
@@ -12,48 +12,23 @@ class Solution:
         
         then it's just answering the requests
         """
-        
-        starts = {}
-        ends = {}
-        
-        for start, end in requests:
-            if start not in starts:
-                starts[start] = 0
-                
-            if end not in ends:
-                ends[end] = 0
-                
-            starts[start] += 1
-            ends[end] += 1
-            
-        starting = min(starts)
-        ending = max(ends)
-        counts = [0] * len(nums)
-        count = 0
-        
-        
-        for idx in range(len(nums)):
-            count += starts.get(idx, 0)
-            counts[idx] = count
-            count -= ends.get(idx, 0)
-            
-        indices = [idx for idx in range(len(nums))]
-        indices.sort(key = lambda idx: counts[idx])
-        nums.sort()
-        new_nums = [0] * len(nums)
-        
-        for idx, indice in enumerate(indices):
-            new_nums[indice] = nums[idx]
-            
-        prefix_sum = [0] * (len(nums) + 1)
-        
-        for idx, val in enumerate(new_nums):
-            prefix_sum[idx + 1] = prefix_sum[idx] + val
-            
+        N = len(nums)
         total = 0
+        nums.sort()
+        counts = [0] * (N + 1)
         
         for start, end in requests:
-            total += prefix_sum[end + 1] - prefix_sum[start]
+            counts[end + 1] -= 1
+            counts[start] += 1
+            
+        for idx in range(1, N + 1):
+            counts[idx] += counts[idx - 1]
+        
+        counts.pop()
+        counts.sort()
+        
+        for count, value in zip(counts,nums):
+            total += count * value
             
         return total % (10 ** 9 + 7)
         
