461d897e57cb11e537a7ca9c12d363f82e62c32b
Henok Matheas Kebede <82048614+Henok-Matheas@users.noreply.github.com>
2023-10-04 11:01:02 +0300

    Time: 42 ms (68.25%), Space: 16.5 MB (26.57%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -0,0 +1,87 @@
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        """
+        [1, 2, 3, 4, 5]
+        [0, 1, 2, 3, 4]
+        
+        2 removed
+        
+        [0, 2, 3, 4]
+        
+        
+        to explain my understanding it goes something like this
+        
+        
+        [0, 1, 2, 3, 4]
+        
+        first we go to 2
+        
+        now the new array becomes
+        
+        [2, 3, 4, 0]
+        
+        or in other words
+        
+        
+        do the same thing for (n - 1, k) but add k to it and % n
+        
+        [0, 1, 2, 3] + 2 % n
+        
+        [2, 3, 4, 0]
+        
+        now next we pick 2 again
+        
+        [2, 3, 0] + 2 % n
+        
+        [4, 0, 2]
+        
+        [0, 1 , 2] + 2
+        
+        
+        
+        0 + 2 % 5
+        
+        3 + 2 % 5
+        
+        2 + 2 % 5
+        
+        4 + 2 % 5
+        
+        1 + 2 % 5
+        
+        k = 2
+        
+        2 removed
+        
+        [1, 3, 4, 5]
+        k = 2
+        
+        4 removed
+        
+        [1, 3, 5]
+        
+        1 removed
+        
+        [3, 5]
+        
+        5 removed
+        
+        [3]
+        return 3
+        
+        the bruteforce answer would be to to append 
+        
+        what if we had a linked list and a dictionary of indexes
+        
+        
+        
+        """
+        
+        
+        def helper(size, k):
+            if size == 1:
+                return 0
+            
+            return (helper(size - 1, k) + k) % size
+        
+        return helper(n , k) + 1
