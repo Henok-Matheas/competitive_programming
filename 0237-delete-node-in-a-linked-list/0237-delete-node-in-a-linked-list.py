b6356699a147e1f2ab45da8067d909db285832bb
Henok Matheas Kebede <82048614+Henok-Matheas@users.noreply.github.com>
2024-05-05 21:54:21 +0300

    Time: 42 ms (29.44%), Space: 16.7 MB (98.22%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.py
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.py
@@ -10,24 +10,13 @@ class Solution:
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.
         
-        we will have node and next
-        
-        node.val = next.val
-        node = node.next
-        next = next.next
-        
-        finally we will make node.next = none
+        bridging
         
         
-        we will never be given the last node so it works
         """
-        next_ = node.next
+        node.val = node.next.val
         
-        while node:
-            node.val = next_.val
-            next_ = next_.next
-            if not next_:
-                node.next = None
-            node = node.next
+        node.next = node.next.next
+        
             
         
