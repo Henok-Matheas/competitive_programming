89c7450fcbc9184e5a4dd7dc2f6c23d17859078a
Henok Matheas Kebede <82048614+Henok-Matheas@users.noreply.github.com>
2024-11-01 20:23:55 +0300

    Time: 152 ms (14.85%), Space: 73.9 MB (6.49%) - LeetHub

--- 0381-insert-delete-getrandom-o1-duplicates-allowed/0381-insert-delete-getrandom-o1-duplicates-allowed.py
+++ 0381-insert-delete-getrandom-o1-duplicates-allowed/0381-insert-delete-getrandom-o1-duplicates-allowed.py
@@ -1,36 +1,64 @@
 class RandomizedCollection:
 
     def __init__(self):
-        self.dictionary = defaultdict(set)
+        ## similar to the non duplicate one but instead of a dict which holds an index it will hold a set of indexes
+        ## when inserting we add the size of the array to the dict set
+        ## when removing
+            ## we will go to the dict set and choose one index and pop
+            ## we will go to the array and then
+                ## get value at last index and get last_val
+                ## go to dict[last_val].pop(last_index)
+                ## dict[last_val].add(val_index)
+                ## array.pop
+        self.dict = {}
         self.array = []
+        
 
     def insert(self, val: int) -> bool:
-        validity = False if self.dictionary[val] else True
+        is_new = False
+        
+        if val not in self.dict:
+            is_new = True
+            self.dict[val] = set()
             
-        self.dictionary[val].add(len(self.array))
+        self.dict[val].add(len(self.array))
         self.array.append(val)
         
-        return validity
+        return is_new
+        
 
     def remove(self, val: int) -> bool:
-        
-        if not self.dictionary[val]:
+        ## we will go to the dict set and choose one index and pop
+            ## we will go to the array and then
+                ## get value at last index and get last_val
+                ## go to dict[last_val].pop(last_index)
+                ## dict[last_val].add(val_index)
+                ## array.pop
+        if val not in self.dict:
             return False
         
-        index = self.dictionary[val].pop()
-        self.array[index], self.array[-1] = self.array[-1], self.array[index]
+        val_idx = self.dict[val].pop()
+        last_idx = len(self.array) - 1
+        last_val = self.array[-1]
+        
+        self.array[val_idx] = last_val
         self.array.pop()
         
-        if index < len(self.array):
-            self.dictionary[self.array[index]].remove(len(self.array))
-            self.dictionary[self.array[index]].add(index)
         
+        self.dict[last_val].add(val_idx)
+        self.dict[last_val].remove(last_idx)
+            
+        if len(self.dict[val]) == 0:
+            self.dict.pop(val)
+            
         return True
+        
 
     def getRandom(self) -> int:
-        index = random.randint(0, len(self.array) - 1)
+        idx = random.randint(0, len(self.array) - 1)
         
-        return self.array[index]
+        return self.array[idx]
+        
 
 
 # Your RandomizedCollection object will be instantiated and called as such:
