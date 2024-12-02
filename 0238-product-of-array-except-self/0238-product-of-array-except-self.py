21c1116abb4af49b623be5e8515a4cbef729c869
Henok Matheas Kebede <82048614+Henok-Matheas@users.noreply.github.com>
2024-12-02 18:13:56 +0300

    Time: 19 ms (85.95%), Space: 24.4 MB (54.71%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,24 +1,16 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        """
-        you just need a right product array
-        
-        
-        for every number we will have a left product using prefix sum kind of thing and then with the right product we can 
-        
-        if we do it in the same array it will be
-        """
-        product = [1] * len(nums)
-        right, left = 1, 1
-        
+        r_prod = [1] * (len(nums) + 1)
+        answer = []
+        prod = 1
         for idx in range(len(nums) - 1, -1, -1):
-            product[idx] = right
-            right *= nums[idx]
-            
-            
+            r_prod[idx] = prod
+            prod *= nums[idx]
+        
+        prod = 1
         for idx, num in enumerate(nums):
-            product[idx] *= left
-            left *= num
+            answer.append(prod * r_prod[idx])
+            prod *= num
             
-        return product
+        return answer
         
